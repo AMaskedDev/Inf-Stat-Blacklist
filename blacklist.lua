192fe182983d725a1e3d449e46731b059f16a384
AMaskedDev <153927002+AMaskedDev@users.noreply.github.com>
2024-07-30 22:11:04 +0300

    Update blacklist.lua

--- blacklist.lua
+++ blacklist.lua
@@ -2,14 +2,5 @@ local bannedPlayers = {"SprouteRL"}
 
 local thisPlayer = game:GetService("Players").LocalPlayer
 if table.find(bannedPlayers, thisPlayer.Name) then 
-  if not Settings.IsPointSlotANamekian then 
-      if player.Character.Race.Value == "Namekian" then
-          TalkNPC(game:GetService("Workspace").FriendlyNPCs["Character Slot Changer"], 0.5, "Yes", "k", Settings.Slot)
-          task.wait(6)
-      end
-  else
-      TalkNPC(game:GetService("Workspace").FriendlyNPCs["Character Slot Changer"], 0.5, "Yes", "k", Settings.Slot)
-      task.wait(6)
-  end
-  print("ok banned")
+  thisPlayer:Kick("You are blacklisted. sry lol")
 end
