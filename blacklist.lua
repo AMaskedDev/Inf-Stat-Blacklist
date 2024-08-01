3c60c0cbe6fe5e5823e4a4ce850c7e8e7dd7b185
AMaskedDev <153927002+AMaskedDev@users.noreply.github.com>
2024-08-01 21:52:09 +0300

    Update blacklist.lua

--- blacklist.lua
+++ blacklist.lua
@@ -1,4 +1,4 @@
-local allowedPlayers = {"SprouteRL", "tyuzulizae", "doodooaron", "jafro1721, "AndroidSupermo"}
+local allowedPlayers = {"SprouteRL", "tyuzulizae", "doodooaron", "jafro1721, "AndroidSupermo", "ttv_kolosllabjan"}
 
 local thisPlayer = game:GetService("Players").LocalPlayer
 if not table.find(allowedPlayers, thisPlayer.Name) then 
